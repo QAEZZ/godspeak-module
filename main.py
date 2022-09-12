db595ccc14b7aad52bebbec46264694abce41980
QÆZZ <65181690+QAEZZ@users.noreply.github.com>
2022-09-12 11:14:57 -0500

    something

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 
 class godspeak():
@@ -9,23 +10,43 @@ class godspeak():
         self.run()
 
     def getWords(self):
-        with open("./data/vocab.txt", "r") as god:
+        with open("./data/vocabList.json", "r") as god:
+            data = json.load(god)
+            #print(data)
             self.words = []
-            for word in god:
-                self.words.append(word.rstrip("\n"))
-                #self.words = word[random.randint(0, 10)]
-        
-        return self.words
-
-    def listify(self):
-        with open("./data/vocab.txt", "r") as god:
-          for word in god:
-            self.words.append(word.rstrip("/n"))
+            count = 0
+            cont = True
+            while cont is True:
+                if count < self.amount:
+                    self.words.append(data[random.randint(0, 100)].rstrip("\n"))
+                else:
+                    cont = False
+                count += 1
 
         return self.words
-    
+
     def run(self):
         print(self.getWords())
 
 
+def listify():
+    with open("./data/vocab.txt", "r") as god:
+        words_listed = []
+        for word in god:
+            words_listed.append(word.rstrip("\n"))
+
+        with open("./data/vocabList.txt", "w") as lord:
+            lord.write(str(words_listed))
+
+    return words_listed
+
+
+def jsonify():
+    with open("./data/vocabList.json", "w") as f:
+        list = listify()
+        json.dump(list, f)
+
+
+# print(listify())
+# jsonify()
 godspeak(10)
